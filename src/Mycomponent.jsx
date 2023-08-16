94993efd8965202819f20fb006d7a3924c881f90
Irzan321 <mhmmdirzan321@gmail.com>
2023-08-16 12:49:49 +0700

    Update audio button

--- src/Mycomponent.jsx
+++ src/Mycomponent.jsx
@@ -1,14 +1,20 @@
 import "/src/assets/style/Mycomponent.css";
 import { useRef, useState } from "react";
-import audio from "../src/assets/audio/NeeckDeep.mp4";
+import audio1 from "../src/assets/audio/NeeckDeep.mp4";
+import audio2 from "../src/assets/audio/NDX.mp4";
 import Typewriter from "typewriter-effect";
 
 function Mycomponent() {
-  const audioRef = useRef(null);
+  const audioRef1 = useRef(null);
+  const audioRef2 = useRef(null);
   const [count, setCount] = useState(0);
 
-  const playAudio = () => {
-    audioRef.current.play().catch(() => {});
+  const playAudio1 = () => {
+    audioRef1.current.play().catch(() => {});
+  };
+
+  const playAudio2 = () => {
+    audioRef2.current.play().catch(() => {});
   };
 
   return (
@@ -29,10 +35,19 @@ function Mycomponent() {
           </button>
         </div>
         <p className="title-count">Click on the Count</p>
+        <div className="btn-audio">
+          <button onClick={playAudio1}>Neck Deep</button>
+          <button onClick={playAudio2}>NDX</button>
+        </div>
+        <p className="title-music">Please Choose Music</p>
         <p>&#9400; IRZAN</p>
       </section>
-      <audio ref={audioRef} autoPlay>
-        <source src={audio} type="audio/mp4" />
+      <audio ref={audioRef1}>
+        <source src={audio1} type="audio/mp4" />
+        Your browser does not support the audio element.
+      </audio>
+      <audio ref={audioRef2}>
+        <source src={audio2} type="audio/mp4" />
         Your browser does not support the audio element.
       </audio>
     </>
